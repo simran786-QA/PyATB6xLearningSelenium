af01d5caf15b9da400593adbefd67b54ff920a46
simran786-QA <smrntester@gmail.com>
2026-01-12 01:20:33 +0530

    open a page by using Selenium.

---
+++ scr/ex_01_Selenium_Basics/test_Selenium_03.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+import allure
+import pytest
+
+
+@allure.title("Verify that we are able to open a page by using Selenium.")
+@allure.description("We will open a page and verify that it is getting opened by using Selenium.")
+def test_first_tc():
+    # Selenium 4
+    driver = webdriver.Firefox()
+    driver.get("https://thetestingacademy.com")
+    print(driver.title)
+    assert driver.title == "TheTestingAcademy | Learn Software Testing and Automation Testing"
