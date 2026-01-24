7a97c6ea1c5257f7345020725fa1a62ba88ff9d4
simran786-QA <smrntester@gmail.com>
2026-01-24 21:34:55 +0530

    Verify that we are able to open a page by using Selenium.

--- scr/ex_01_Selenium_Basics/test_Selenium_01.py
+++ scr/ex_01_Selenium_Basics/test_Selenium_01.py
@@ -1,42 +1,17 @@
-# -------------------------------------------------------------
 # Author: Simran Shaikh
 # Topic: Selenium Test – Open Web Page & Verify Title
-# -------------------------------------------------------------
 
 from selenium import webdriver
 import allure
 import pytest
 
-
-# -------------------------------------------------------------
 # Test Case: Verify page opens successfully using Selenium
-# -------------------------------------------------------------
-
 @allure.title("Verify that we are able to open a page by using Selenium.")
 @allure.description("We will open a page and verify that it is getting opened by using Selenium.")
 def test_first_tc():
-
-    # ---------------------------------------------------------
-    # Step 1: Launch Edge browser
-    # ---------------------------------------------------------
-    driver = webdriver.Edge()
-
-    # ---------------------------------------------------------
-    # Step 2: Open the target URL
-    # ---------------------------------------------------------
-    driver.get("https://thetestingacademy.com")
-
-    # ---------------------------------------------------------
-    # Step 3: Fetch and print page title
-    # ---------------------------------------------------------
-    print(driver.title)
-
-    # ---------------------------------------------------------
-    # Step 4: Validate page title using assertion
-    # ---------------------------------------------------------
+    driver = webdriver.Edge()  # Step 1: Launch Edge browser
+    driver.get("https://thetestingacademy.com")  #Step 2: Open the target URL
+    print(driver.title) # Step 3: Fetch and print page title
     assert driver.title == "TheTestingAcademy | Learn Software Testing and Automation Testing"
-
-    # ---------------------------------------------------------
-    # Step 5: Close the browser
-    # ---------------------------------------------------------
-    driver.quit()
+    # Step 4: Validate page title using assertion
+    driver.quit() #Step 5: Close the browser
